b6cba6b3c06aeec36b83d01a08dd975318c3d0d9
JT Lu <alohajt94@gmail.com>
2020-02-05 20:20:33 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(arr)
+  i = 0 
+  arr2 = []
+  
+  while i < arr.length do 
+    arr2.push(yield(arr[i]))
+    i += 1
+  end 
+  return arr2
+end 
+
+def reduce(arr, sv=nil)
+  if sv then 
+    sum = sv
+    i = 0 
+  else 
+    sum = arr[0]
+    i = 1 
+  end 
+  while i < arr.length do 
+    sum = yield(sum, arr[i])
+    i += 1
+  end 
+  return sum
+end
