dcfc3cf88f594fddad92fb82bff2afee61da7e25
Jeremy Hart <jerome215@comcast.net>
2020-02-10 15:16:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,24 @@
-def fadi_map_result{ [ num1,num2,mun3,num4;return_negative]}
-    fadi_map = [1, 2, 3, -9]
-    return_negative = *-1
+def map(s)
+  new = []
+  i = 0
+  while i < s.length
+    new.push(yield(s[i]))
+    i += 1
+  end
+  new
+end
 
-    Result_negative = {|n| n * -1})
-    puts result_negative 
-  end 
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
+end
